a03a08a8b76e72c076dbc36095e1e4f95f5ded3d
keith <keith.woolterton@gmail.com>
2021-05-19 08:12:32 +0800

    beefed up card factory

--- card_dungeon/model/card_factory.py
+++ card_dungeon/model/card_factory.py
@@ -45,6 +45,23 @@ class CardFactory():
             e.is_quick = row["Quick"] > 0
             e.new_card_count = row["Extra Card"]
 
+            e.attacks[CardFeature.ATTACK_MELEE] = row["Melee Attack"]
+            e.attacks[CardFeature.ATTACK_MAGIC] = row["Magic Attack"]
+            e.blocks[CardFeature.BLOCK_MELEE] = row["Melee Block"]
+            e.blocks[CardFeature.BLOCK_MAGIC] = row["Magic Block"]
+
+            heal_value = row["Heal"]
+            if heal_value > 0:
+                heal_outcome = Outcome._value2member_map_[row["Heal Condition"]]
+                e.heals[heal_outcome] = row["Heal"]
+
+            effect = row["Effect"]
+            if effect is not None:
+                effect_name = Effect._value2member_map_[effect]
+                effect_outcome = Outcome._value2member_map_[row["Effect Condition"]]
+                e.effects[effect_outcome] = effect_name
+
+
         else:
             print(f"Can't find entity {name} in factory!")
 
